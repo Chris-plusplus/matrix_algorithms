26496dd579afc3b97bd6500fcdda902111c04688
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-11-04 17:44:02 +0100

    szacowanie do 2

--- programs/szacowanie.cpp
+++ programs/szacowanie.cpp
@@ -1,10 +1,11 @@
+#include <array>
 #include <format>
 #include <iostream>
 #include <map>
 #include <ranges>
 
 int main() {
-	auto map = std::map<unsigned int, std::pair<unsigned int, unsigned int>>();
+	auto map = std::map<unsigned int, std::array<double, 4>>();
 
 	size_t size;
 	std::cout << "sample size: ";
@@ -12,39 +13,52 @@ int main() {
 
 	for (size_t i = 0; i != size; ++i) {
 		unsigned int N;
-		unsigned int t1;
-		unsigned int t2;
+		double t1;
+		double t2;
+		double t3;
+		double t4;
 
-		std::cin >> N >> t1 >> t2;
-		map[N] = { t1, t2 };
+		std::cin >> N >> t1 >> t2 >> t3 >> t4;
+		map[N] = { t1, t2, t3, t4 };
 	}
 
-	auto get = [&map](unsigned int N) -> std::pair<unsigned int, unsigned int> {
+	auto get = [&map](unsigned int N) -> std::array<double, 4> {
 		if (map.contains(N)) {
 			return map[N];
 		} else {
 			auto ub = map.upper_bound(N);
 			auto ubm1 = std::prev(ub);
 
-			return { (ub->second.first + ubm1->second.first) / 2, (ub->second.second + ubm1->second.second) / 2 };
+			return { (ub->second[0] + ubm1->second[0]) / 2,
+					 (ub->second[1] + ubm1->second[1]) / 2,
+					 (ub->second[2] + ubm1->second[2]) / 2,
+					 (ub->second[3] + ubm1->second[3]) / 2 };
 		}
 	};
 
 	double sum1 = 0;
 	double sum2 = 0;
+	double sum3 = 0;
+	double sum4 = 0;
 
 	unsigned int counter = 0;
 	for (auto&& [key, val] : std::views::reverse(map)) {
-		if (key > 50) {
+		if (key > 5) {
 			++counter;
 			auto got = get(key / 2);
-			sum1 += double(val.first / got.first);
-			sum2 += double(val.second / got.second);
+			sum1 += double(val[0] / got[0]);
+			sum2 += double(val[1] / got[1]);
+			sum3 += double(val[2] / got[2]);
+			sum4 += double(val[3] / got[3]);
 		}
 	}
 	sum1 /= counter;
 	sum2 /= counter;
+	sum3 /= counter;
+	sum4 /= counter;
 
 	std::cout << sum1 << '\n';
 	std::cout << sum2 << '\n';
+	std::cout << sum3 << '\n';
+	std::cout << sum4 << '\n';
 }
